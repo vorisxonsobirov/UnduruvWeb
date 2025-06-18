32e89a357090e839ac1e8319ba231ec5e57dd5fa
Vorisxon <vorisxon.me@gmail.com>
2025-06-18 17:52:53 +0500

    first test for proxy.js part 4

--- Proxy.js
+++ Proxy.js
@@ -1,21 +1,17 @@
-// proxy.js
-const express = require("express");
-const fetch = require("node-fetch");
-const cors = require("cors");
-const app = express();
-const PORT = 4000;
+import express from "express";
+import fetch from "node-fetch";
+import cors from "cors";
 
+const app = express();
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 
 app.get("/api/debtors", async (req, res) => {
-  const username = process.env.API_USERNAME;
-  const password = process.env.API_PASSWORD;
+  const url = process.env.TARGET_URL || "http://nasiya.mxsoft.uz/demo_nasiya/hs/GPScontrol/apigps/getdebtors?page=1&count=500";
+  const username = process.env.API_USERNAME || "api";
+  const password = process.env.API_PASSWORD || "123";
   const auth = Buffer.from(`${username}:${password}`).toString("base64");
-
-  //   const url = "http://nasiya.mxsoft.uz/demo_nasiya/hs/GPScontrol/apigps/getdebtors?page=1&count=500&branch_id=1";
-  const url = "http://nasiya.mxsoft.uz/demo_nasiya/hs/GPScontrol/apigps/getdebtors?page=1&count=500";
-
 
   try {
     const response = await fetch(url, {
@@ -27,11 +23,11 @@ app.get("/api/debtors", async (req, res) => {
     const data = await response.json();
     res.json(data);
   } catch (error) {
-    console.error("Ошибка прокси:", error);
-    res.status(500).json({ error: "Ошибка при получении данных" });
+    console.error("Proxy error:", error);
+    res.status(500).json({ error: "Fetch failed" });
   }
 });
 
 app.listen(PORT, () => {
-  console.log(`Прокси сервер запущен: http://localhost:${PORT}`);
+  console.log(`Proxy server running on port ${PORT}`);
 });
